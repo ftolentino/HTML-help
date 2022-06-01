b021d02668e26144eb65e53c7f3e6a074e93ba0d
ftolentino <filmer.tolentino@gmail.com>
2022-05-31 23:35:06 -0700

    ADD walrus image, ADD jquery-3.6.0.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-jQuery("h1").click(function() {
-  alert("This is a heading.");
-});
-
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
-
-jQuery("img").click(function() {
-  alert("This is an image.");
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a heading.");
+  });
+  
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
+  
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
 });
